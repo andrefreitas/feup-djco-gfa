270cd934915a89edd8a90f81032ba367a7f76e7f
Joaquim Barros <jruirbarros@gmail.com>
2014-05-21 23:14:24 +0100

    Added script to control animations. Not fully working.

---
+++ Assets/Scripts/PersonAnimationScript.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+private var thirdPersonController : ThirdPersonController;
+private var personAnimation : Animation;
+private var animDisabled : boolean = false;
+
+function Start () {
+	thirdPersonController = GetComponent(ThirdPersonController);
+	personAnimation = GetComponent(Animation);
+}
+
+function Update () {
+	if(thirdPersonController.IsJumping()) {
+		if(!animDisabled) {
+			Debug.Log("JUMPING");
+			personAnimation.enabled = false;
+			animDisabled = true;
+		}
+	}
+	
+	else if(!thirdPersonController.IsMoving()) {
+		if(!animDisabled) {
+			Debug.Log("STOPPED");
+			personAnimation.enabled = false;
+			animDisabled = true;
+		}
+	}
+	
+	else {
+		if(animDisabled) {
+			Debug.Log("MOVING");
+			personAnimation.enabled = true;
+			animDisabled = false;
+		}
+	}
+}
