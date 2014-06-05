4ca97dae1edfd19f0806808855e372390817172e
Joaquim Barros <jruirbarros@gmail.com>
2014-06-05 11:57:37 +0100

    Door rotates.

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class DoorTrigger : MonoBehaviour {
 
+	public GameObject door;
+	public GameObject doorSwitch;
+	public long maxCycles = 600;
+	public long cycles = 0;
+	private bool pressed = false;
+	public float angularVel = 0.5f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,10 +17,23 @@ public class DoorTrigger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (pressed) {
+
+			door.transform.Rotate(Vector3.up * (-angularVel));
+
+			if(cycles < maxCycles) {
+				Debug.Log(cycles);
+				cycles++;
+			}
+
+			else pressed = false;
+		}
 	
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Debug.Log ("Pressed");
+		cycles = 0;
+		pressed = true;
 	}
 }
