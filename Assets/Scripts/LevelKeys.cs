dd20207ac41bd62486479584a20f1fc62cd0cd03
Joaquim Barros <jruirbarros@gmail.com>
2014-06-09 22:28:28 +0100

    Added keys to manage application: - ESC to exit - F1 to restart

---
+++ Assets/Scripts/LevelKeys.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelKeys : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+				Debug.Log ("END");
+				Application.Quit ();
+		}
+		else if (Input.GetKey (KeyCode.F1))
+				Application.LoadLevel (Application.loadedLevel);
+	}
+}
