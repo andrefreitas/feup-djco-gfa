35ca9446e336868331262ee05f5f2f9a719bd67c
Ana Gomes <acgg.gomes@gmail.com>
2014-06-15 15:35:32 +0100

    Adicionado script para o cubo cair.

---
+++ Assets/Scripts/fallCubeTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fallCubeTrigger : MonoBehaviour {
+	public GameObject fallCube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		fallCube = GameObject.Find("fallCube");
+		fallCube.rigidbody.AddForce (Vector3.forward * 1000);
+		Debug.Log ("Detetou colisao");
+	}
+}
