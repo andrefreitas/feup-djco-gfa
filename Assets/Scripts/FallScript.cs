f33fac5bd1334b8a13abfd3f162d88ddd92f8ff0
Joaquim Barros <jruirbarros@gmail.com>
2014-06-17 00:42:11 +0100

    Lives working also when falling from floor 1

---
+++ Assets/Scripts/FallScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(LivesScript.DecreaseLives())
+			Application.LoadLevel (Application.loadedLevel);
+		
+		else Application.Quit (); // substituir por intro
+	}
+}
