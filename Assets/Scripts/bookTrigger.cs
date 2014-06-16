883913b0eb55f06c9d58b8543eff902e22cc11ee
André Freitas <p.andrefreitas@gmail.com>
2014-06-16 12:20:58 +0100

    Implementado o desafio da chave a encontrar o livro. Falta corrigir o bug do trigger que só funciona uma vez.

---
+++ Assets/Scripts/bookTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class bookTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	public static bool bookFound;
+	public GameObject bookText;
+
+	// Use this for initialization
+	void Start () {
+		bookFound = false;
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log ("Book found: " + bookTrigger.bookFound);
+		bookText = GameObject.Find ("bookText");
+		if (!bookTrigger.bookFound) {
+			bookText.guiText.enabled = true;
+		} else {
+			bookText.guiText.enabled = true;
+			bookText.guiText.text = "Encontraste!";
+		}
+		Destroy (gameObject);
+	}
+
+}
