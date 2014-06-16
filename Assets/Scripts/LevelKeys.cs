3bd827f59202d45ea1df69c2e076abdb046fa609
Joaquim Barros <jruirbarros@gmail.com>
2014-06-17 00:32:35 +0100

    Added lives to floor 0 and 1

--- Assets/Scripts/LevelKeys.cs
+++ Assets/Scripts/LevelKeys.cs
@@ -11,10 +11,13 @@ public class LevelKeys : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.Escape)) {
-				Debug.Log ("END");
-				Application.Quit ();
-		}
-		else if (Input.GetKey (KeyCode.F1))
+			Debug.Log ("END");
+			Application.Quit ();
+		} else if (Input.GetKey (KeyCode.F1)) {
+			if(LivesScript.DecreaseLives())
 				Application.LoadLevel (Application.loadedLevel);
+
+			else Application.Quit (); // substituir por intro
+		}
 	}
 }
---
+++ Assets/Scripts/LivesScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LivesScript : MonoBehaviour {
+
+	private static int lives = 3;
+	public static GameObject textLives;
+
+	// Use this for initialization
+	void Start () {
+		textLives = GameObject.Find ("Lives_info");
+		UpdateTextInfo ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private static void UpdateTextInfo() {
+		textLives.gameObject.guiText.text = "Lives: " + lives;
+		Debug.Log ("Lives: " + lives);
+	}
+
+	public static void IncreaseLives() {
+		lives++;
+		UpdateTextInfo ();
+	}
+
+	// true if ok, false if end
+	public static bool DecreaseLives() {
+		lives--;
+		UpdateTextInfo ();
+
+		if (lives == 0)
+			return false;
+
+		return true;
+	}
+
+	public static int GetLives() {
+		return lives;
+	}
+}
